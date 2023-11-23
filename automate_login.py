d94dac72ef492819937403085060179c9c12ffc1
petrioteer <petrioteer007@gmail.com>
2023-11-23 10:10:32 +0530

    added .env for credentials

--- automate_login.py
+++ automate_login.py
@@ -1,19 +1,19 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-username = os.getenv('USERNAME')
+username = os.getenv('REGNO')
 password = os.getenv('PASSWORD')
 #setting location of webdriver -- set your own location 
 service = Service('C:\chromedriver.exe')
 
 options = Options()
-#options.add_argument('--headless=new')
+options.add_argument('--headless=new')
 driver = webdriver.Chrome(service=service, options=options)
 
 driver.implicitly_wait(2)
@@ -26,13 +26,13 @@ def opening_browser():
     Select_Box_Name = driver.find_element(by=By.NAME, value = "username")
     Select_Box_Name.click()
     
-    #here in place of "xyz" just replace it with your Reg. Number in double quotes.. eg. "xyz" --> "12327845"
+
     Select_Box_Name.send_keys(username)
     
     Select_Box_Name = driver.find_element(by=By.NAME, value = "password")
     Select_Box_Name.click()
     
-    #Again, replace "xyz" with your internet password that you set in your UMS.
+
     Select_Box_Name.send_keys(password)
 
     Select_Box_Name = driver.find_element(by=By.ID, value = "agreepolicy")
@@ -41,7 +41,7 @@ def opening_browser():
 
     click_send = driver.find_element(by=By.ID, value ="loginbtn")
     click_send.click()
-    
+
     print("Login Successful, Exiting...")
 
 opening_browser()
